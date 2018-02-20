6f3b75f5d96a277cbb5175eab3bfb5d8738269c2
lhgioia <lhgioia@gmail.com>
2018-02-20 15:01:58 -0800

    Added qsub script to submit meta analysis

---
+++ src/meta_analysis_launcher.sh
@@ -0,0 +1,8 @@
+###################################
+#### METAL FDR Launcher Script ####
+###################################
+
+## run from kidneyMetaAS directory
+qsub -N meta_analysis ./src/meta_analysis.sh
+
+
