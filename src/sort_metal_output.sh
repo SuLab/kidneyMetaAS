878b08068f9ac775ddcd418e3875b51244327b65
lhgioia <lhgioia@gmail.com>
2018-03-26 16:36:26 -0700

    Updated to relative directories

--- src/sort_metal_output.sh
+++ src/sort_metal_output.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-in_dir="/gpfs/group/su/lhgioia/kidneyMetaAS/results/meta"
-out_dir="/gpfs/group/su/lhgioia/kidneyMetaAS/results/meta"
+in_dir="./results/meta"
+out_dir="./results/meta"
 
 grep -v "nane" ${in_dir}/metal_output1.txt | grep "+++" |  sort -k6,6g > ${out_dir}/metal_output_sorted.tsv
 
