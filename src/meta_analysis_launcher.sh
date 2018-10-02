0661efe6f2cc7ac7b4bcd4261f2f877c333eff09
lhgioia <lhgioia@gmail.com>
2018-10-02 11:15:53 -0700

    Removed main analysis files from base src directory

--- src/meta_analysis_launcher.sh
+++
@@ -1,8 +0,0 @@
-###################################
-#### METAL FDR Launcher Script ####
-###################################
-
-## run from kidneyMetaAS directory
-qsub -N meta_analysis -o ./results/logs/meta_analysis_standard_output.txt -e ./results/logs/meta_analysis_standard_error.txt ./src/meta_analysis.sh
-
-
--- src/sort_metal_output.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-in_dir="./results/meta"
-out_dir="./results/meta"
-
-grep -v "nane" ${in_dir}/metal_output1.txt | grep "+++" |  sort -k6,6g > ${out_dir}/metal_output_sorted.tsv
-
